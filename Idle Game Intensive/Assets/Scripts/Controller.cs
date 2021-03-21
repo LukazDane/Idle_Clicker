640a0fd8ad62406e75a6a18b4ce052771a8963d5
Luke <deandrevidal@aol.com>
2021-03-21 00:38:55 -0700

    fixed infinite variable error

--- Idle Game Intensive/Assets/Scripts/Controller.cs
+++ Idle Game Intensive/Assets/Scripts/Controller.cs
@@ -3,15 +3,26 @@ using TMPro;
 
 public class Controller : MonoBehaviour
 {
+    public Data data;
+    public Data data2;
+
     public TMP_Text manaText;
 
-    public double mana;
+    public void Start()
+    {
+        data = new Data();
+        data2 = new Data();
+    }
+
     public void Update()
     {
-        manaText.text = mana + " Mana";
+        data.mana = 10;
+        manaText.text = data.mana + " Mana\n";
+        manaText.text += data2.mana + " Mana";
+
     }
     public void GenerateMana()
     {
-        mana +=1;
+        // mana +=1;
     }
 }
---
+++ Idle Game Intensive/Assets/Scripts/Data.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+public class Data
+{
+    public double mana;
+    public Data()
+    {
+        mana = 0;
+    }
+}
