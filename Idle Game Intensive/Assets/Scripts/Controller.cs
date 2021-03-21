23fc94dd902a45c3685ecc4edc58a27f2c29eb17
Luke <deandrevidal@aol.com>
2021-03-21 01:14:32 -0700

    [BreakInfinity] Big Double

--- Idle Game Intensive/Assets/Scripts/Controller.cs
+++ Idle Game Intensive/Assets/Scripts/Controller.cs
@@ -4,25 +4,21 @@ using TMPro;
 public class Controller : MonoBehaviour
 {
     public Data data;
-    public Data data2;
 
     public TMP_Text manaText;
 
-    public void Start()
+    private void Start()
     {
         data = new Data();
-        data2 = new Data();
     }
 
-    public void Update()
+    private void Update()
     {
-        data.mana = 10;
-        manaText.text = data.mana + " Mana\n";
-        manaText.text += data2.mana + " Mana";
+        manaText.text = data.mana + " Mana";
 
     }
     public void GenerateMana()
     {
-        // mana +=1;
+        data.mana +=1;
     }
 }
